594e4853489d7b87d292a62054496853dd38058d
Emmanuel Isaac <deejecks@live.com>
2016-10-03 22:06:27 +0100

    Update staticfiles setting

--- realtime_calculator/settings.py
+++ realtime_calculator/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
