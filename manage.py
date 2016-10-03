a04b6f0b06ef75917d81d356625ade1e05a555a2
Emmanuel Isaac <deejecks@live.com>
2016-10-03 22:13:59 +0100

    Django settings module for prod

--- manage.py
+++ manage.py
@@ -2,8 +2,10 @@
 import os
 import sys
 
+import envvars
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtime_calculator.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", envvars.get('DJANGO_SETTINGS_MODULE', "realtime_calculator.settings"))
 
     from django.core.management import execute_from_command_line
 
