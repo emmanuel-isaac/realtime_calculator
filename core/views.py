b9a0492d079846c56b55c5f3dfe50dc3bc3244f2
Emmanuel Isaac <deejecks@live.com>
2016-10-03 21:54:08 +0100

    Refactor calculator elements and remove redundant comments

--- core/views.py
+++ core/views.py
@@ -6,8 +6,10 @@ from .models import CalculatorData
 def home(request):
     results = CalculatorData.objects.order_by("-timestamp")[:10]
     operators = ["-", "*", "/", "+"]
+    numbers = ["{}".format(num) for num in range(10)]
+    calc_elements = operators + numbers
 
     return render(request, "home.html", {
         'results': results,
-        'operators': operators,
+        'elements': calc_elements,
     })
